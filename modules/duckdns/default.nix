fcb462e4e99ba8aec4e013c19692be6d47ca2149
Gavin Downard <gavin.downard@runbox.com>
2021-11-27 17:25:30 -0800

    System: change duckdns service to read from /root

--- modules/duckdns/default.nix
+++ modules/duckdns/default.nix
@@ -1,8 +1,6 @@
 { config, lib, pkgs, ... }:
 
-let
-  duckdns_hostname = "";
-  duckdns_token = "";
+let duckdns_hostname = "gdown";
 in {
   systemd.services.duckdns = {
     description = "DuckDNS update job";
@@ -12,7 +10,8 @@ in {
       PATH="${pkgs.curl}/bin:${pkgs.util-linux}/bin:$PATH"
       logger -t DuckDNS "Updating DuckDNS entries"
       EXITCODE=0
-      OUTPUT=$(curl -k -s "https://www.duckdns.org/update?domains=${duckdns_hostname}&token=${duckdns_token}&ip=")
+      TOKEN="$(cat /root/nix/duckdns-token.txt)"
+      OUTPUT=$(curl -k -s "https://www.duckdns.org/update?domains=${duckdns_hostname}&token=$TOKEN&ip=")
       logger -t DuckDNS "$OUTPUT"
       if [ "$OUTPUT" == "KO" ]; then
           logger -t DuckDNS "You should check if your domain/token is correct because the server responded negatively!"
