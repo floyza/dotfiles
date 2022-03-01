e770003df4f801f1cc453e9a86b6a5964533480b
Gavin Downard <gavin.downard@runbox.com>
2022-03-01 09:04:45 -0800

    System: use local sway

--- flake.nix
+++ flake.nix
@@ -8,11 +8,33 @@
     nur.url = "github:nix-community/NUR";
     emacs-overlay.url = "github:nix-community/emacs-overlay";
     ssbm.url = "github:djanatyn/ssbm-nix";
+    sway.url = "/home/gavin/src/sway";
+    sway.flake = false;
   };
-  outputs = { self, home-manager, nur, nixpkgs, emacs-overlay, ssbm, ... }:
+  outputs =
+    { self, home-manager, nur, nixpkgs, emacs-overlay, ssbm, sway, ... }:
     let
       common = { pkgs, config, ... }: {
-        nixpkgs.overlays = [ nur.overlay emacs-overlay.overlay ];
+        nixpkgs.overlays = [
+          nur.overlay
+          emacs-overlay.overlay
+          (self: super: {
+            sway-unwrapped = super.sway-unwrapped.overrideAttrs (oldAttrs: {
+              version = "custom-git";
+              src = sway;
+            });
+            wlroots = super.wlroots.overrideAttrs (oldAttrs: {
+              version = "git";
+              src = self.fetchFromGitLab {
+                domain = "gitlab.freedesktop.org";
+                owner = "wlroots";
+                repo = "wlroots";
+                rev = "511f137f8fb245e4877d83a0846294091373eba1";
+                sha256 = "sha256-yvB3V5OnS4IdkiR4co9xUEhhGy9Tslsg7Hr7B2PjZfs=";
+              };
+            });
+          })
+        ];
         nix.registry.nixpkgs.flake = nixpkgs;
       };
     in {
