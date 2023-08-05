be1a4b0a3b1772ea30ea89d4fc8e47d16ded96f2
Gavin Downard <gavin.downard@runbox.com>
2023-08-05 02:34:51 +0000

    remove relative paths

--- flake.nix
+++ flake.nix
@@ -4,29 +4,21 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-23.05";
     nixpkgs-master.url = "github:nixos/nixpkgs/master";
-    nixpkgs-custom.url = "/home/gavin/src/nixpkgs";
     home-manager.url = "github:nix-community/home-manager/release-23.05";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
     nur.url = "github:nix-community/NUR";
     emacs-overlay.url = "github:nix-community/emacs-overlay";
     emacs-overlay.inputs.nixpkgs.follows = "nixpkgs";
-    # ssbm.url = "github:djanatyn/ssbm-nix";
-    ssbm.url = "/home/gavin/src/ssbm-nix";
 
     # My secrets are currently stored plaintext in my nix store, but I at least don't want to commit them to git
     # so I split them off here
     secrets.url = "/home/gavin/src/dotfiles/secrets";
     secrets.flake = false;
   };
-  outputs = { self, home-manager, nur, nixpkgs, nixpkgs-master, nixpkgs-custom
-    , emacs-overlay, ssbm, secrets, ... }@attrs: {
+  outputs = { self, home-manager, nur, nixpkgs, nixpkgs-master, emacs-overlay, secrets, ... }@attrs: {
       nixosConfigurations = let
         system = "x86_64-linux";
         master = (import nixpkgs-master {
-          inherit system;
-          config.allowUnfree = true;
-        });
-        custom = (import nixpkgs-custom {
           inherit system;
           config.allowUnfree = true;
         });
@@ -57,8 +49,6 @@
 
             ./common/settings.nix
             ./modules/japanese
-            ./modules/ssbm
-            ssbm.nixosModule # this seems wrong to have outside of ./modules/ssbm
             ./common/configuration.nix
             {
               home-manager.users.gavin = {
