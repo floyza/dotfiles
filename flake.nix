48c3d3ed123cdc74b04aedda00fedd7635b3e9cc
Gavin Downard <gavin.downard@runbox.com>
2023-08-04 22:32:50 -0700

    Restore ssbm

--- flake.nix
+++ flake.nix
@@ -9,13 +9,15 @@
     nur.url = "github:nix-community/NUR";
     emacs-overlay.url = "github:nix-community/emacs-overlay";
     emacs-overlay.inputs.nixpkgs.follows = "nixpkgs";
+    ssbm.url = "github:jumper149/ssbm-nix/mbedtls2";
 
     # My secrets are currently stored plaintext in my nix store, but I at least don't want to commit them to git
     # so I split them off here
     secrets.url = "/home/gavin/src/dotfiles/secrets";
     secrets.flake = false;
   };
-  outputs = { self, home-manager, nur, nixpkgs, nixpkgs-master, emacs-overlay, secrets, ... }@attrs: {
+  outputs = { self, home-manager, nur, nixpkgs, nixpkgs-master, emacs-overlay
+    , ssbm, secrets, ... }@attrs: {
       nixosConfigurations = let
         system = "x86_64-linux";
         master = (import nixpkgs-master {
@@ -47,6 +49,8 @@
             (sys-path + "/configuration.nix")
             (sys-path + "/hardware-configuration.nix")
 
+            ./modules/ssbm
+            ssbm.nixosModule # this seems wrong to have outside of ./modules/ssbm
             ./common/settings.nix
             ./modules/japanese
             ./common/configuration.nix
