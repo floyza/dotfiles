d8cabcfaff07b34361318176431811738b9b2165
Gavin Downard <me@floyza.com>
2024-04-28 14:55:50 -0700

    Re-add samba

--- systems/dreadnought/configuration.nix
+++ systems/dreadnought/configuration.nix
@@ -86,5 +86,51 @@
     };
   };
 
+  networking = {
+    firewall.allowedTCPPorts = [ 5357 ]; # samba-wsdd
+    firewall.allowedUDPPorts = [ 3702 ]; # samba-wsdd
+    # possibly required for our samba discovery (https://wiki.archlinux.org/index.php/Samba#.22Browsing.22_network_fails_with_.22Failed_to_retrieve_share_list_from_server.22)
+    firewall.extraCommands = ''
+      iptables -t raw -A OUTPUT -p udp -m udp --dport 137 -j CT --helper netbios-ns
+    '';
+  };
+
+  users.users.samba = {
+    isSystemUser = true;
+    group = "samba";
+  };
+  users.groups.samba = { };
+
+  services.samba = {
+    enable = true;
+    openFirewall = true;
+    securityType = "user";
+    enableNmbd = false; # we use wsdd instead
+    enableWinbindd = true; # need to look into more
+    extraConfig = ''
+      workgroup = WORKGROUP
+      protocol = SMB3
+      # server string = smbnix
+      # netbios name = smbnix # we aren't using nmbd so I think we don't need this
+      hosts allow = 192.168.0.0/24 10.42.0.0/24 127.0.0.1
+      hosts deny = 0.0.0.0/0
+      guest account = samba
+      map to guest = bad user
+    '';
+    shares = {
+      family = {
+        comment = "Public Share";
+        path = "/shares/Public";
+        browseable = "yes";
+        "read only" = "no";
+        "guest ok" = "yes";
+        "create mask" = "0644";
+        "directory mask" = "0755";
+      };
+    };
+  };
+
+  services.samba-wsdd = { enable = true; };
+
   system.stateVersion = "21.11";
 }
