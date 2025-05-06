86a05aa858c98bcbefac96c0e0693fc2405e0470
floyza <me@floyza.com>
2025-05-06 11:56:07 -0700

    cruiser: config touchpad

--- systems/cruiser/home.nix
+++ systems/cruiser/home.nix
@@ -1,5 +1,12 @@
 { config, lib, pkgs, ... }:
 
 {
+  wayland.windowManager.sway.config = {
+    input."2:8:AlpsPS/2_ALPS_GlidePoint" = {
+      accel_profile = "adaptive";
+      pointer_accel = "0.2";
+      tap = "enabled";
+    };
+  };
   home.stateVersion = "24.11";
 }
