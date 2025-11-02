48aa897c8ec188f58f3aa321792e622effecfe4f
floyza <me@floyza.com>
2025-11-01 18:37:20 -0700

    add ssh key

--- systems/dreadnought/configuration.nix
+++ systems/dreadnought/configuration.nix
@@ -76,6 +76,7 @@
   users.users.gavin.openssh.authorizedKeys.keys = [
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAID94ckwO1qnrewCT8QBou/8x+Wj7IUg9x+1/qn25IhVz gavin@Acer-Nitro-5" # acer laptop
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPkz94VB3wSbpCOIvaQ1f9ElZtmkfwQUK2dLdNJ33REa gavin.downard@runbox.com" # hp 655 laptop
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOhZaKnC6FdEL2MBZoPVpXmH0VzIr3QzmASYlFH/EYe0 me@floyza.com"
   ];
 
   services.openssh.enable = true;
