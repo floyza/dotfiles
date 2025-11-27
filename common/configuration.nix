7eebaded0360493f5adfa75e54e1d9f27f9fb490
floyza <me@floyza.com>
2025-11-27 11:56:17 -0800

    add adb

--- common/configuration.nix
+++ common/configuration.nix
@@ -30,6 +30,8 @@
     })
   ];
 
+  programs.adb.enable = true;
+
   # The global useDHCP flag is deprecated, therefore explicitly set to false here.
   # Per-interface useDHCP will be mandatory in the future, so this generated config
   # replicates the default behaviour.
